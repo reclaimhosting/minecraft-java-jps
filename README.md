3bb4aa7a0fe7dd7c9993815e43a44eaf2caaec54
timmmmyboy <genial@gmail.com>
2020-06-05 09:24:31 -0400

    Add scripts to test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# minecraft-java-jps
+# Minecraft Java Server for Reclaim Cloud
+In Minecraft, players explore a blocky, procedurally-generated 3D world, and may discover and extract raw materials, craft tools, build structures or earthworks, and depending on game mode, can fight computer-controlled "mobs", as well as either cooperate with or compete against other players in the same world. Minecraft servers allow players to play online or via a local area network with other people. This installs the Java variant of Minecraft server. For information on compatibility see [https://minecraft.gamepedia.com/Java_Edition](https://minecraft.gamepedia.com/Java_Edition)
+
+## Deploy to Reclaim Cloud
+[Click here to deploy to Reclaim Cloud](https://app.my.reclaim.cloud/?app=minecraft-java)
+
+## Installation Instructions
---
+++ scripts/addEndpoint.js
@@ -0,0 +1,49 @@
+//@req(nodeGroup, name, port)
+
+var PROTOCOL = getParam("protocol", "TCP"),
+    APPID = getParam("TARGET_APPID"),
+    SESSION = getParam("session"),
+    bEndPointsEnabled,
+    sSuccessText = "",
+    nNodesCount,
+    oEnvInfo,
+    oResp,
+    i;
+
+oResp = jelastic.billing.account.GetQuotas("environment.endpoint.enabled");
+
+if (!oResp || oResp.result != 0) {
+    return oResp;
+}
+
+bEndPointsEnabled = oResp.array[0].value;
+
+oEnvInfo = jelastic.environment.environment.GetEnvInfo(APPID, session);
+
+if (!oEnvInfo || oEnvInfo.result != 0) {
+    return oEnvInfo;
+}
+
+nNodesCount = oEnvInfo.nodes.length;
+
+if (bEndPointsEnabled) {
+    for (i = 0; i < nNodesCount; i += 1) {
+        if (oEnvInfo.nodes[i].nodeGroup == nodeGroup) {
+            oResp = jelastic.environment.environment.AddEndpoint(APPID, session, oEnvInfo.nodes[i].id, port, PROTOCOL, name);
+
+            if (!oResp || oResp.result != 0) {
+                return oResp;
+            }
+        }
+    }
+
+    sSuccessText = "To access your Minecraft server, use the server name  <b>${env.domain}" + oResp.object.publicPort + "</b>";
+} else {
+    sSuccessText = "Public port access is only possible with a billing account. Please upgrade to access your minecraft server remotely and reinstall.";
+}
+
+return {
+    result: "success",
+    message: sSuccessText,
+    email: sSuccessText
+};
